cdc42cdacaa37dc58757eef966a0e50caf1c4cb7
Antonio Aldisio <aldisiofilho@gmail.com>
2022-05-01 16:44:30 -0300

    add novas tabelas e perfis

--- TF_1B1_Apaga_Final_por_Dupla_AntonioFilho.sql
+++ TF_1B1_Apaga_Final_por_Dupla_AntonioFilho.sql
@@ -3,7 +3,7 @@
 --         SCRIPT APAGA
 --
 -- Data Criacao ...........: 15/04/2022
--- Autor(es) ..............: Caio César Oliveira, Antônio Aldísio de Sousa Alves Ferreira Filho 
+-- Autor(es) ..............: Caio César Oliveira, Antônio Aldísio de Sousa Alves Ferreira Filho, Bianca Sofia de Oliveira, Daniel Porto
 -- Banco de Dados .........: MySQL 8.0
 -- Base de Dados (nome) ...: trabalhofinal1
 --
@@ -18,22 +18,24 @@
 
 USE trabalhofinal1;
 
-DROP TABLE RELATORIO;
-DROP TABLE habilidadesTrabalhadas;
-DROP TABLE especialistasFornecendoAtendimentoAtualmente;
+DROP TABLE trabalha;
+DROP TABLE HABILIDADE;
 DROP TABLE medicamentos;
-DROP TABLE telefones;
-DROP TABLE medicamentosDuranteGravidez;
-DROP TABLE doencasDuranteGravidez;
-DROP TABLE conhecimentos;
-DROP TABLE dificuldades;
-DROP TABLE ajudantesNasTarefas;
-DROP TABLE problemasSaudeFisica;
-DROP TABLE casosNaFamiliaDeDoenca;
-DROP TABLE medicamentosAoNascer;
 DROP TABLE doencasAoNascer;
-DROP TABLE ACOMPANHAMENTO;
-DROP TABLE CONTRATACAO;
+
+DROP TABLE especialistasFornecendoAtendimentoAtualmente;
+DROP TABLE medicamentosAoNascer;
+DROP TABLE casosNaFamiliaDeDoenca;
+DROP TABLE problemasSaudeFisica;
+DROP TABLE ajudantesNasTarefas;
+DROP TABLE dificuldades;
+DROP TABLE conhecimentos;
+DROP TABLE doencasDuranteGravidez;
+DROP TABLE medicamentosDuranteGravidez;
+DROP TABLE RELATORIO;
+DROP TABLE SESSAO;
+DROP TABLE CONTRATO;
+DROP TABLE telefones;
 DROP TABLE PACOTE;
 DROP TABLE RESPONSAVEL;
 DROP TABLE DOENCAINFANCIA;
@@ -42,6 +44,9 @@ DROP TABLE ANAMNESE;
 DROP TABLE DOCUMENTO;
 DROP TABLE PSICOPEDAGOGO;
 
-DROP USER admin;
-DROP USER psicopedagogo;
-DROP USER usuario;
+DROP USER bianca;
+DROP USER caio;
+DROP USER rozana;
+DROP USER flavio;
+DROP USER maria;
+DROP USER joao;
--- TF_1B1_Controle_Final_por_Dupla_AntonioFilho.sql
+++ TF_1B1_Controle_Final_por_Dupla_AntonioFilho.sql
@@ -3,29 +3,46 @@
 --         SCRIPT CONTROLE
 --
 -- Data Criacao ...........: 27/04/2022
--- Autor(es) ..............: Caio César Oliveira, Antônio Aldísio de Sousa Alves Ferreira Filho 
+-- Autor(es) ..............: Caio César Oliveira, Antônio Aldísio de Sousa Alves Ferreira Filho, Bianca Sofia de Oliveira, Daniel Porto
 -- Banco de Dados .........: MySQL 8.0
 -- Base de Dados (nome) ...: trabalhofinal1
---
+-- Ultimas Alteracoes
+--                   01/05/22 => Adequação das permissoes
 -- PROJETO => 01 Base de Dados
 --         => 23 Tabelas
 --         => 03 Usuarios
 -- ---------------------------------------
 
--- USUARIO
-CREATE USER admin
+-- PERFIL ADMINISTRADOR
+CREATE USER bianca
 IDENTIFIED BY 'adminSupperar';
 
-CREATE USER psicopedagogo
+CREATE USER caio
+IDENTIFIED BY 'adminSupperar';
+
+-- PERFIL PSICOPEDAGOGO
+CREATE USER rozana
 IDENTIFIED BY 'psicopedagogoSuperar';
 
-CREATE USER usuario
-IDENTIFIED BY 'usuarioSuperar';
+CREATE USER flavio
+IDENTIFIED BY 'psicopedagogoSuperar';
+
+-- PERFIL RESPONSAVEL
+CREATE USER maria
+IDENTIFIED BY 'responsavelSuperar';
+
+CREATE USER joao
+IDENTIFIED BY 'responsavelSuperar';
 
 
--- PRIVILEGIO
-GRANT ALL PRIVILEGES ON trabalhofinal1.* TO admin;
+-- PRIVILEGIO ADMINISTRADOR
+GRANT ALL PRIVILEGES ON trabalhofinal1.* TO bianca;
+GRANT ALL PRIVILEGES ON trabalhofinal1.* TO caio;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON trabalhofinal1.* TO psicopedagogo;
+-- PRIVILEGIO PsicoPedagogo
+GRANT SELECT, INSERT, UPDATE, DELETE ON trabalhofinal1.* TO rozana;
+GRANT SELECT, INSERT, UPDATE, DELETE ON trabalhofinal1.* TO flavio;
 
-GRANT SELECT ON trabalhofinal1.* TO usuario;
+-- PRIVILEGIO Responsavel
+GRANT SELECT ON trabalhofinal1.* TO maria;
+GRANT SELECT ON trabalhofinal1.* TO joao;
